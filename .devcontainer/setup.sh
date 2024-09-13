7b9b387164ee0ab4e613830f4623f84d1aa13b22
Ben Greenberg <hello@yalladevrel.com>
2024-09-13 13:14:58 +0300

    zsh out of setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -13,6 +13,8 @@ apt-get install -y \
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y 
+. "$HOME/.cargo/env"
+
 rustup install nightly
 rustup component add rustfmt
 rustup component add rustfmt --toolchain nightly
@@ -21,10 +23,3 @@ rustup component add clippy --toolchain nightly
 
 cargo install cargo-expand
 cargo install cargo-edit
-
-## setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
